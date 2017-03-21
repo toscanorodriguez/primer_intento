bb86c2b8f4a53ce517ef52edca8e35f1b35753dd
usuario <usuario>
2017-03-21 12:44:21 +0100

    for añadido

--- src/main/java/com/mgcss/practica1/App.java
+++ src/main/java/com/mgcss/practica1/App.java
@@ -9,5 +9,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "¡Hola Mundo, que tal?!" );
+        for (int i = 0; i < args.length; i++) {
+            String string = args[i];
+            
+        }
     }
 }
